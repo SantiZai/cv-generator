be4d7caaed121794d7db3b59a99d1e6706f0e11c
SantiZai <santyagozaidan@gmail.com>
2024-08-11 22:45:00 -0300

    pdf description

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -33,17 +33,25 @@ export const generateDoc = (formInfo: {
     experience,
   } = formInfo;
 
-  const PDF = new jsPDF();
+  const PDF = new jsPDF({format: "a4"});
 
   PDF.setFontSize(32);
-  PDF.text(firstname.toString(), 105, 10, { align: "center" });
-  PDF.setFontSize(20);
+  PDF.text(`${firstname.toString()} ${lastname.toString()}`, 105, 10, {
+    align: "center",
+  });
+  PDF.setFontSize(18);
   PDF.text(
-    `${location.toString()} ${sociallink.toString()} ${phonenumber.toString()} ${email.toString()}`,
+    `${location.toString()} ° ${sociallink.toString()} ° ${phonenumber.toString()} ° ${email.toString()}`,
     105,
     20,
     { align: "center" }
   );
+
+  PDF.line(10, 25, 200, 25)
+
+  PDF.text(description.toString(), 10, 35, {maxWidth: 190})
+
+  PDF.line(10, 50, 200, 50)
 
   return PDF;
 };
