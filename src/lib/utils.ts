83c5a9860b611e695bd53835eca4a128efcd4176
SantiZai <santyagozaidan@gmail.com>
2024-08-11 22:31:50 -0300

    generate doc util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,17 +2,48 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 import { jsPDF } from "jspdf";
+import { Education, Experience } from "./interfaces";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const generatePDF = (info: { firstname: string; lastname: string }) => {
-  const doc = new jsPDF();
+export const generateDoc = (formInfo: {
+  firstname: string;
+  lastname: string;
+  location: string;
+  description: string;
+  email: string;
+  phonenumber: string;
+  sociallink: string;
+  image: string | ArrayBuffer | null;
+  education: Education[];
+  experience: Experience[];
+}) => {
+  const {
+    firstname,
+    lastname,
+    location,
+    description,
+    email,
+    phonenumber,
+    sociallink,
+    image,
+    education,
+    experience,
+  } = formInfo;
 
-  doc.setFontSize(24)
-  doc.text(info.firstname, 10, 10)
-  doc.text(info.lastname, 10, 20)
+  const PDF = new jsPDF();
 
-  doc.save("test.pdf")
+  PDF.setFontSize(32);
+  PDF.text(firstname.toString(), 105, 10, { align: "center" });
+  PDF.setFontSize(20);
+  PDF.text(
+    `${location.toString()} ${sociallink.toString()} ${phonenumber.toString()} ${email.toString()}`,
+    105,
+    20,
+    { align: "center" }
+  );
+
+  return PDF;
 };
